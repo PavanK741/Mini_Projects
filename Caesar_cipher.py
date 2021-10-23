3e0afb2c7c72347f5d33c3cd67633f9d5cb5bc5e
PavanK741 <77979048+PavanK741@users.noreply.github.com>
2021-10-23 21:44:34 +0530

    Add files via upload

---
+++ Caesar_cipher.py
@@ -0,0 +1,50 @@
+from art import logo
+
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def restart(choice):
+    if choice=="yes":
+
+      direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+      text = input("Type your message:\n").lower()
+      shift = int(input("Type the shift number:\n"))
+
+      if shift>26:
+        shift_amount=shift%26
+
+      caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+      choice=input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+      restart(choice)
+     
+    elif choice=="no":
+      print("... Terminating the Program ...")
+    exit
+
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+
+    #TODO-3: What happens if the user enters a number/symbol/space?
+    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+    #e.g. start_text = "meet me at 3"
+    #end_text = "•••• •• •• 3"
+    
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+  choice=input("Type 'yes' if you want to go again. Otherwise type 'no'.")
+  restart(choice)
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+caesar(cipher_direction=direction,start_text=text,shift_amount=shift)
